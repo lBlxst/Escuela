2146f70b4d38694778ead02a7454740984f83710
lBlxst <164287515+lBlxst@users.noreply.github.com>
2025-06-01 17:24:07 -0600

    Add files via upload

---
+++ tictactoe/tictactoe.js
@@ -0,0 +1,47 @@
+const board = document.querySelector('.board');
+const cells = document.querySelectorAll('.cell'); // Cambié el nombre para mayor claridad
+const display = document.getElementById('winner');
+
+let currentPlayer = "X";
+let gameActive = true;
+let boardState = ['', '', '', '', '', '', '', '', ''];
+
+function hayGanador() {
+    const combinacionesGanadoras = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8],
+        [0, 3, 6], [2, 5, 8], [1, 4, 7],
+        [0, 4, 8], [2, 4, 6]
+    ];
+    for (const combinacion of combinacionesGanadoras) {
+        const [a, b, c] = combinacion;
+        if (boardState[a] && boardState[a] === boardState[b] && boardState[a] === boardState[c]) {
+            return boardState[a];
+        }
+    }
+    return null;
+}
+
+function cellClick(event) {
+    const cell = event.target; 
+    const index = cell.dataset.index; 
+    if (!gameActive || boardState[index]) return;
+
+    boardState[index] = currentPlayer;
+    cell.textContent = currentPlayer; 
+    cell.classList.add('taken');
+
+    const winner = hayGanador();
+
+    if (winner) {
+        display.textContent = `El ganador es ${winner}`;
+        gameActive = false;
+    } else if (!boardState.includes('')) {
+        display.textContent = `Los dos jugadores han empatado`;
+        gameActive = false;
+    } else {
+        currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+    }
+}
+
+
+cells.forEach(cell => cell.addEventListener('click', cellClick));
